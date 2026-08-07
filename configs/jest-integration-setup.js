bd3bd0f9cdb56ca33d3e5a00ce95ba6b8de0e2d8
ym-4 <payym.25@ichat.sp.edu.sg>
2026-08-07 09:48:10 +0800

    fixing merge conflicts

--- configs/jest-integration-setup.js
+++ configs/jest-integration-setup.js
@@ -3,5 +3,4 @@ const { execSync } = require('child_process');
 module.exports = async () => {
   console.log('Running database reset for integration tests...');
   execSync('dotenv -e .env.test -- node scripts/reset.js', { stdio: 'inherit' });
-  //  execSync('node scripts/reset.js', { stdio: 'inherit', env: process.env });
 };
